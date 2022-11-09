e45415fbdb9c8b255dca9035edf4d14a19479af8
Kevin Page <kjp020@bucknell.edu>
2022-11-09 09:37:31 -0500

    committing for push to main

---
+++ src/main/java/org/team6/SolitaireModel.java
@@ -0,0 +1,28 @@
+/* *****************************************
+ * CSCI205 -Software Engineering and Design
+ * Fall2022* Instructor: Prof. Brian King
+ *
+ * Name: Kevin Page
+ * Section: 9:00
+ * Date: 11/8/2022
+ * Time: 10:14 AM
+ *
+ * Project: csci205_final_prject
+ * Package: org.team6
+ * Class: Pile
+ *
+ * Description: Class representing the entire game of Solitaire
+ *
+ * *****************************************/
+package org.team6;
+
+import java.util.ArrayList;
+
+public class SolitaireModel {
+    ArrayList<Pile> Piles = new ArrayList<>();
+
+    public static void createPiles(){
+
+    }
+
+}
