18b6b29692d02fa7736aa91332168da5b0917b80
Alejandro Vargas Altamirano <aava001@bucknell.edu>
2022-11-11 09:26:41 -0500

    Talon class, first pass, complete

---
+++ src/main/java/org/team6/Talon.java
@@ -0,0 +1,82 @@
+/* ******************************************
+ * CSCI205 -Software Engineering and Design
+ * Fall2022* Instructor: Prof. Brian King
+ *
+ * Name: Alejandro Vargas
+ * Section: 9:00
+ * Date: 11/8/2022
+ * Time: 10:14 AM
+ *
+ * Project: csci205_final_prject
+ * Package: org.team6
+ * Class: Pile
+ *
+ * Description: Class representing the Talon pile
+ *
+ ********************************************
+ */
+
+package org.team6;
+
+import java.util.ArrayList;
+
+/**
+ * A class to encapsulate the talon object in a solitaire game
+ */
+public class Talon {
+    /**
+     * Array list holding all of the cards in the talon
+     */
+    private ArrayList<Card> cards;
+
+    /**
+     * Constructs a new Talon with an empty cards list
+     */
+    public Talon(){
+        this.cards = new ArrayList<>();
+    }
+
+    /**
+     * Adds a card from the stock to the talon
+     * @param drawnCard - the card drawn from the stock that is to be added
+     */
+    public void addCard(Card drawnCard){
+        this.cards.add(drawnCard);
+    }
+
+    /**
+     * Returns the top card on the talon
+     * @return the card at the top of the talon or null if the talon is empty
+     */
+    public Card getTopCard(){
+        if(this.cards.size() > 0){
+            return this.cards.get(this.cards.size()-1);
+        }
+        else{
+            return null;
+        }
+    }
+
+    /**
+     * Returns the top card on the talon and removes it, or returns null if talon is empty
+     * @return the top card on the talon or null if the talon is empty
+     */
+    public Card drawCard(){
+        Card out = getTopCard();
+        if(out!= null){
+            this.cards.remove(this.cards.size()-1);
+        }
+        return out;
+    }
+
+    /**
+     * Empties the talon so that the cards can be returned to the stock
+     */
+    public void emptyTalon(){
+        this.cards = new ArrayList<>();
+    }
+
+    public ArrayList<Card> getCards() {
+        return cards;
+    }
+}
