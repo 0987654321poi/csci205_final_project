1d0e86fb83b60d07d780e5e25f444b67534f6a20
Alejandro Vargas Altamirano <aava001@bucknell.edu>
2022-11-14 09:52:46 -0500

    Foundations class, added get and remove card methods

--- src/main/java/org/team6/Foundations.java
+++ src/main/java/org/team6/Foundations.java
@@ -65,4 +65,55 @@ public class Foundations {
             return false;
         }
     }
+
+    /**
+     * Meathod to pull a card from the foundation so that it can be moved back onto
+     * a tableau pile
+     * @param type, the suit of the foundation you want to pull from
+     * @return the card at the top of that foundation
+     */
+    public Card getCard(suit type){
+        Card out = null;
+        if(type.equals(suit.HEART)){
+            if(this.hearts.size() !=0) {
+                out = getTopHeart();
+                this.hearts.remove(this.hearts.size() - 1);
+            }
+        }
+        else if(type.equals(suit.DIAMOND)){
+            if(this.diamonds.size() != 0) {
+                out = getTopDiamond();
+                this.diamonds.remove(this.diamonds.size() - 1);
+            }
+        }
+        else if(type.equals(suit.SPADE)){
+            if(this.spades.size() !=0) {
+                out = getTopSpade();
+                this.spades.remove(this.spades.size() - 1);
+            }
+        }
+        else{
+            if(this.clubs.size() != 0) {
+                out = getTopClub();
+                this.clubs.remove(this.clubs.size() - 1);
+            }
+        }
+        return out;
+    }
+
+    public Card getTopHeart() {
+        return hearts.get(hearts.size()-1);
+    }
+
+    public Card getTopDiamond() {
+        return diamonds.get(diamonds.size()-1);
+    }
+
+    public Card getTopSpade() {
+        return spades.get(spades.size()-1);
+    }
+
+    public Card getTopClub() {
+        return clubs.get(clubs.size()-1);
+    }
 }
