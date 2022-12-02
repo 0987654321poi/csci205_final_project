8cb8ceb69975a775b9d286a1fd6265eb74a2562e
Gordon Rose <gar018@bucknell.edu>
2022-12-02 09:42:01 -0500

    updated documentation for Stock

--- src/main/java/org/team6/Stock.java
+++ src/main/java/org/team6/Stock.java
@@ -22,12 +22,13 @@ import java.util.ArrayList;
 
 public class Stock {
     /**
-     * Arraylist to store the cards in the stock
+     * Arraylist of cards to store the cards in the stock
      */
     ArrayList<Card> cards;
 
     /**
-     * Creates a new stock with all of the cards that need to be in the stock when the
+     * Stock Constructor class
+     * Creates a new stock with all cards that need to be contained within the stock when the
      * game starts
      * @param cards - the cards that need to be added to the stock at the beginning of the game
      */
@@ -36,8 +37,8 @@ public class Stock {
     }
 
     /**
-     * Draws the top card from the stock
-     * @return
+     * Draws the top card from the stock and returns it
+     * @return out - the top card of the stock being returned
      */
     public Card drawCard(){
         Card out = null;
@@ -50,6 +51,7 @@ public class Stock {
 
     /**
      * Resets the stock with more cards
+     * this occurs when the talon has received all the cards contained in the stock
      * @param newStock - the array list of cards which will become the new stock
      */
     public void resetStock(ArrayList<Card> newStock){
@@ -57,7 +59,7 @@ public class Stock {
     }
 
     /**
-     *
+     * simple boolean that determine if the stock is empty (contains no Cards)
      * @return boolean, is the stock empty
      */
     public boolean isEmpty(){
@@ -65,7 +67,7 @@ public class Stock {
     }
 
     /**
-     * Returns the top card but does not remove it
+     * Returns the top card but does not remove it (effectively 'peeks' the top card)
      * @return the top card in the stock
      */
     public Card getTopCard(){
